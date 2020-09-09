13d1e15220a2bb143cb506cbe3527a58e6cac5e9
Esme Hotston Moore <esmexhotstonmoore@deloitte.co.uk>
2020-09-09 11:30:04 +0100

    refactor: Starting to split into smaller dumb components

---
+++ src/Book.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import BookshelfChanger from './BookshelfChanger'
+
+const Book = ({bookCoverUrl, bookCoverHeight, bookTitle, bookAuthor}) => {
+    return (
+        <div className="book">
+                <div className="book-top">
+                  <div className="book-cover" style={{ width: 128, height: bookCoverHeight, backgroundImage: bookCoverUrl}}></div>
+                  <BookshelfChanger/>
+                </div>
+                <div className="book-title">{bookTitle}</div>
+                <div className="book-authors">{bookAuthor}</div>
+              </div>
+    )
+}
+
+export default Book
---
+++ src/Bookshelf.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Book from './Book'
+
+const Bookshelf = ({shelf, books}) => {
+
+    const booksOnShelf = books.filter(book => shelf.books.includes(book.bookTitle))
+
+    return (
+        <div className="bookshelf">
+        <h2 className="bookshelf-title">{shelf.title}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+          
+               {booksOnShelf.map(book => {
+                 return (<li>
+                 <Book
+                 bookCoverUrl={book.bookCoverUrl}
+                bookCoverHeight={book.bookCoverHeight}
+              bookTitle={book.bookTitle}
+              bookAuthor={book.bookAuthor}/>
+              </li>)})}
+              
+          </ol>
+        </div>
+      </div>
+    )
+}
+
+export default Bookshelf
